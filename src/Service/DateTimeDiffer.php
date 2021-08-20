17f1555832a9948807e50e9aa7d90078f87a238c
Golovko Artem <artem.golovko2008@gmail.com>
2021-08-20 11:24:29 +0300

    Fix a bug

--- src/Service/DateTimeDiffer.php
+++ src/Service/DateTimeDiffer.php
@@ -3,15 +3,18 @@
 namespace App\Service;
 
 use Carbon\Carbon;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class DateTimeDiffer implements DateTimeDifferInterface
 {
     private string $locale;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, ContainerInterface $container)
     {
         $this->locale = $requestStack->getCurrentRequest() ? $requestStack->getCurrentRequest()->getLocale() : 'ru';
+        $locales = (array)$container->getParameter('locales');
+        $this->locale = (in_array($this->locale, $locales)) ?: 'ru';
     }
 
     public function getDiff($dateTime): string
